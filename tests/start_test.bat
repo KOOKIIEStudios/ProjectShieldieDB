10a09e1f407103b3d0bb55b426b054d8cb1149cf
KOOKIIE <KookiieStudios@outlook.com>
2020-11-12 14:57:38 +0800

    Add batch file for automated test

---
+++ tests/start_test.bat
@@ -0,0 +1,7 @@
+:: This script allows the user to launch the tests without needing use command line
+echo "This script will start the ProjectShieldieDB test sequence."
+echo "Starting up virtual environment..."
+call venv\scripts\activate.bat
+echo "Attempting to test the capabilities of SowrdieDB..."
+py.test
+echo "Test concluded."
