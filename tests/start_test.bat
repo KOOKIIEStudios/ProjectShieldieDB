9331c0e124d90b30ca736fd03ff617bb25ac5532
KOOKIIE <KookiieStudios@outlook.com>
2020-11-12 16:20:56 +0800

    Add @ECHO off

--- tests/start_test.bat
+++ tests/start_test.bat
@@ -1,4 +1,5 @@
 :: This script allows the user to launch the tests without needing use command line
+@ECHO off
 echo "This script will start the ProjectShieldieDB test sequence."
 echo "Starting up virtual environment..."
 call venv\scripts\activate.bat
